40641b8566beee24ba4fe9e0fad28a710e3bc79f
Lenny Parisi <lenny.parisi@hotmail.com>
2017-02-06 11:03:20 -0500

    -Added login screen

--- Assets/Final/Scripts/MeteorManager.cs
+++ Assets/Final/Scripts/MeteorManager.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MeteorManager : MonoBehaviour
 {
-    void Start()
-    {
-        StartCoroutine(ConnectToMeteor());
-    }
+	//Text componets to get user input
+	private Text email, password;
+	//Strings that will hold the actual user input
+	private string emailStr, passwordStr;
+	//Prefab of a score
+	public GameObject SCORE_OBJ;
+	//Array of scores
+	private Player[] scoreArray;
+
+	void Start () 
+	{
+		//Set the email and password text components
+		email = GameObject.Find ("EmailText").GetComponent<Text>();
+		password = GameObject.Find ("PasswordText").GetComponent<Text>();
+
+		//Connect to meteor server
+		StartCoroutine (ConnectToMeteor ());
+	}
 
 
     IEnumerator ConnectToMeteor()
@@ -15,21 +30,21 @@ public class MeteorManager : MonoBehaviour
         Debug.Log("attempting to connect");
         yield return Meteor.Connection.Connect("ws://meteor-tommsy.rhcloud.com:8000/websocket");
         Debug.Log("connected");
-        StartCoroutine(MeteorLogin());
     }
 
-    IEnumerator tests()
-    {
-        var method = Meteor.Method<string>.Call("GetCollection", "PlayerStats");
-        yield return (Coroutine)method;
-        Debug.Log(method.Response);
-    }
+
+	//Logs user in with the given credentials
+	public void Login()
+	{
+		emailStr = email.text;
+		passwordStr = password.text;
+		StartCoroutine(MeteorLogin());
+	}
 
     //Logs user into his/her account
     IEnumerator MeteorLogin()
     {
         Debug.Log("attempting to log in");
-        string emailStr = "lenny", passwordStr = "123";
         var login = Meteor.Accounts.LoginWith(emailStr, passwordStr);
         yield return (Coroutine)login;
         Debug.Log("Ok proceed");
