3dbb586f5fe73079e6a02d0a89d4f2cb6ca7fad8
Lenny Parisi <lenny.parisi@hotmail.com>
2017-02-06 07:58:56 -0500

    -Added meteor-unity package

---
+++ Assets/Final/Scripts/MeteorManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MeteorManager : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ConnectToMeteor());
+    }
+
+
+    IEnumerator ConnectToMeteor()
+    {
+        Debug.Log("attempting to connect");
+        yield return Meteor.Connection.Connect("ws://meteor-tommsy.rhcloud.com:8000/websocket");
+        Debug.Log("connected");
+        StartCoroutine(MeteorLogin());
+    }
+
+    IEnumerator tests()
+    {
+        var method = Meteor.Method<string>.Call("GetCollection", "PlayerStats");
+        yield return (Coroutine)method;
+        Debug.Log(method.Response);
+    }
+
+    //Logs user into his/her account
+    IEnumerator MeteorLogin()
+    {
+        Debug.Log("attempting to log in");
+        string emailStr = "lenny", passwordStr = "123";
+        var login = Meteor.Accounts.LoginWith(emailStr, passwordStr);
+        yield return (Coroutine)login;
+        Debug.Log("Ok proceed");
+        if (Meteor.Accounts.IsLoggedIn)
+        {
+            Debug.Log("login success!");
+            DontDestroyOnLoad(this);
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Failed to log in...");
+            emailStr = "";
+            passwordStr = "";
+            //email.text = "";
+            //password.text = "";
+        }
+    }
+}
